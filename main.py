aef7d428429194b2d0b4af0afb9ca6bd587f06c4
jiahaobao42@gmail.com <jiahaobao42@gmail.com>
2024-12-21 19:00:52 +0800

    packed  THU EAT

---
+++ main.py
@@ -0,0 +1,84 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+import base64
+import json
+import matplotlib.pyplot as plt
+import requests
+import platform
+
+
+def decrypt_aes_ecb(encrypted_data: str) -> str:
+    
+    key = encrypted_data[:16].encode('utf-8')
+    encrypted_data = encrypted_data[16:]
+    encrypted_data_bytes = base64.b64decode(encrypted_data)
+    
+    cipher = AES.new(key, AES.MODE_ECB)
+    
+    decrypted_data = unpad(cipher.decrypt(encrypted_data_bytes), AES.block_size)
+
+    return decrypted_data.decode('utf-8')
+
+idserial = ""
+servicehall = ""
+all_data = dict()
+
+if __name__ == "__main__":
+    # 读入账户信息
+    print("本项目是一个用于统计华清大学学生在食堂（和宿舍）的消费情况的脚本。通过模拟登录华清大学校园卡网站，获取学生在华子食堂的消费记录，并通过数据可视化的方式展示。\n")
+    try:
+        with open("./config.json", "r", encoding='utf-8') as f:
+            account = json.load(f)
+            idserial = account["idserial"]
+            servicehall = account["servicehall"]
+    except Exception as e:
+        print("账户信息读取失败，请重新输入")
+        idserial = input("请输入学号: ")
+        print("访问 https://github.com/leverimmy/THU-Annual-Eat 以获取服务代码的获取教程\n")
+        servicehall = input("请输入服务代码: ")
+        with open("config.json", "w", encoding='utf-8') as f:
+            json.dump({"idserial": idserial, "servicehall": servicehall}, f, indent=4)
+    
+    # 发送请求，得到加密后的字符串
+    url = f"https://card.tsinghua.edu.cn/business/querySelfTradeList?pageNumber=0&pageSize=5000&starttime=2024-01-01&endtime=2024-12-31&idserial={idserial}&tradetype=-1"
+    cookie = {
+        "servicehall": servicehall,
+    }
+    response = requests.post(url, cookies=cookie)
+
+    # 解密字符串
+    encrypted_string = json.loads(response.text)["data"]
+    decrypted_string = decrypt_aes_ecb(encrypted_string)
+
+    # 整理数据
+    data = json.loads(decrypted_string)
+    for item in data["resultData"]["rows"]:
+        try:
+            if item["mername"] in all_data:
+                all_data[item["mername"]] += item["txamt"]
+            else:
+                all_data[item["mername"]] = item["txamt"]
+        except Exception as e:
+            pass
+    all_data = {k: round(v / 100, 2) for k, v in all_data.items()} # 将分转换为元，并保留两位小数
+    print(len(all_data))
+    # 输出结果
+    all_data = dict(sorted(all_data.items(), key=lambda x: x[1], reverse=False))
+    if platform.system() == "Darwin":
+        plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    else:
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.figure(figsize=(12, len(all_data) / 66 * 18))
+    plt.barh(list(all_data.keys()), list(all_data.values()))
+    for index, value in enumerate(list(all_data.values())):
+        plt.text(value + 0.01 * max(all_data.values()),
+                index,
+                str(value),
+                va='center')
+        
+    # plt.tight_layout()
+    plt.xlim(0, 1.2 * max(all_data.values()))
+    plt.title("华清大学食堂消费情况")
+    plt.xlabel("消费金额（元）")
+    plt.savefig("result.png")
+    plt.show()
